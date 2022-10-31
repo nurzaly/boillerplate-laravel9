d5eb5bf24fd81114810bd59058d29038d3b2631b
Nurzaly <nrzaly@gmail.com>
2022-11-01 07:58:49 +0800

    remove announcement

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use App\Domains\Announcement\Services\AnnouncementService;
+//use App\Domains\Announcement\Services\AnnouncementService;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,18 +16,18 @@ class ComposerServiceProvider extends ServiceProvider
      *
      * @param  AnnouncementService  $announcementService
      */
-    public function boot(AnnouncementService $announcementService)
+    public function boot()
     {
         View::composer('*', function ($view) {
             $view->with('logged_in_user', auth()->user());
         });
 
-        View::composer(['frontend.index', 'frontend.layouts.app'], function ($view) use ($announcementService) {
-            $view->with('announcements', $announcementService->getForFrontend());
-        });
+        // View::composer(['frontend.index', 'frontend.layouts.app'], function ($view) use ($announcementService) {
+        //     $view->with('announcements', $announcementService->getForFrontend());
+        // });
 
-        View::composer(['backend.layouts.app'], function ($view) use ($announcementService) {
-            $view->with('announcements', $announcementService->getForBackend());
-        });
+        // View::composer(['backend.layouts.app'], function ($view) use ($announcementService) {
+        //     $view->with('announcements', $announcementService->getForBackend());
+        // });
     }
 }
